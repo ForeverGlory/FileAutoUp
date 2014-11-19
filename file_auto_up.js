508a1dff4cc49fdc26e337d52619b4b1864b24cc
ForeverGlory <foreverglory@qq.com>
2014-11-19 11:50:02 +0800

    更改js判断方式，直接通过传递id判断

--- file_auto_up.js
+++ file_auto_up.js
@@ -1,15 +1,16 @@
 (function ($) {
-  Drupal.behaviors.file_auto_up = {
-    attach: function (context, settings) {
-      $(".file-auto-up", context).each(function () {
-        var $this = $(this);
-        $(".form-file:file", $this).bind("change", function (e) {
-          var files = e.target.files || e.dataTransfer.files;
-          if (files && files.length) {
-            $(".form-submit:submit", $this).trigger("mousedown");
-          }
-        }).trigger("change");
-      });
-    }
-  };
+    Drupal.behaviors.file_auto_up = {
+        attach: function (context, settings) {
+            if (jQuery.type(settings.file_auto_up) == 'array') {
+                jQuery.each(settings.file_auto_up, function (key, value) {
+                    $("#" + value + "-upload", context).bind("change", function (e) {
+                        var files = e.target.files || e.dataTransfer.files;
+                        if (files && files.length) {
+                            $("#" + value + "-upload-button", context).trigger("mousedown");
+                        }
+                    }).trigger("change");
+                });
+            }
+        }
+    };
 })(jQuery);
