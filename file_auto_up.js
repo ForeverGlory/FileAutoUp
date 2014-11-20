763b45b3399464fa5b3b4d24b886f7d9cc754788
ForeverGlory <foreverglory@qq.com>
2014-11-20 16:20:01 +0800

    修复移除后，不能再次自动提交

--- file_auto_up.js
+++ file_auto_up.js
@@ -3,10 +3,10 @@
         attach: function (context, settings) {
             if (jQuery.type(settings.file_auto_up) == 'array') {
                 jQuery.each(settings.file_auto_up, function (key, value) {
-                    $("#" + value + "-upload", context).bind("change", function (e) {
+                    $("#" + value + " :file", context).bind("change", function (e) {
                         var files = e.target.files || e.dataTransfer.files;
                         if (files && files.length) {
-                            $("#" + value + "-upload-button", context).trigger("mousedown");
+                            $(this).next(":submit").trigger("mousedown");
                         }
                     }).trigger("change");
                 });
